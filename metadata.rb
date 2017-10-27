3638545b132306648acdb0159e1c55bdaf06d355
David Alexander <opensource@thelonelyghost.com>
2017-10-27 18:23:15 -0400

    Up-to-date metadata.rb is helpful

--- metadata.rb
+++ metadata.rb
@@ -6,15 +6,12 @@ description 'Installs/Configures tor'
 long_description 'Installs/Configures tor'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
 issues_url 'https://github.com/TranscribersOfReddit/tor-cookbook/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
 source_url 'https://github.com/TranscribersOfReddit/tor-cookbook'
 
+recipe 'tor::default', 'Install ALL the things!'
+recipe 'tor::python', 'Installs/Configures python3, pip, and virtualenv'
+recipe 'tor::redis', 'Installs/Configures Redis server'
+
+supports 'centos', '>= 7.3'
 depends 'yum-ius', '~> 2.2.0'
