0f2d325f6dc96972489c74923d7a117d86f85e87
David Alexander <opensource@thelonelyghost.com>
2017-10-28 16:31:46 -0400

    Bumps to v0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'opensource@thelonelyghost.com'
 license 'All Rights Reserved'
 description 'Installs/Configures tor'
 long_description 'Installs/Configures tor'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/TranscribersOfReddit/tor-cookbook/issues'
 source_url 'https://github.com/TranscribersOfReddit/tor-cookbook'
