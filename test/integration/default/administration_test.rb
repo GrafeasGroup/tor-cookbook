5a4bd57df9256d39e53bf42488a279c91fe40415
David Alexander <opensource@thelonelyghost.com>
2017-10-28 16:00:21 -0400

    Fully-fledged inspec controls
    - Test kitchen uses data bags like the recipe it tests
    - Integration tests are more mature than smoke tests

--- test/integration/default/administration_test.rb
+++ test/integration/default/administration_test.rb
@@ -6,10 +6,16 @@
 # found at http://inspec.io/docs/reference/resources/
 admin_users = attribute('admin_users', default: [], description: 'Default set of admins')
 
-admin_users.each do |username|
-  describe user(username) do
-    it { should exist }
-    its('shell') { should include 'bash' }
-    its('groups') { should include 'sudo' }
+control 'sysadmin-access' do
+  title 'Administrative Users'
+  desc '
+    SysAdmins have sufficient access to do the needful
+  '
+
+  admin_users.each do |username|
+    describe user(username) do
+      it { should exist }
+      its('groups') { should include 'sudo' }
+    end
   end
 end
---
+++ test/integration/default/python_test.rb
@@ -0,0 +1,34 @@
+# # encoding: utf-8
+
+# Inspec test for recipe tor::python
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+control 'modern-python' do
+  title 'Modern python application dependencies'
+  desc '
+    Dependencies required for a modern python application are
+    installed and configured globally
+  '
+
+  %w[python36u python36u-pip python36u-devel].each do |pkg|
+    describe package(pkg) do
+      it { should be_installed }
+    end
+  end
+
+  describe command('python3.6 --version') do
+    its('stdout') { should include '3.6' }
+    its('stderr') { should eq '' }
+  end
+
+  describe command('python3 --version') do
+    its('stdout') { should include '3.4' }
+    its('stderr') { should eq '' }
+  end
+
+  describe command('virtualenv --help') do
+    its('exit_status') { should eq 0 }
+  end
+end
