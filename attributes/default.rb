26521f4effc448524b8c4972fff6ecf722dfe2d8
David Alexander <opensource@thelonelyghost.com>
2017-12-05 22:22:57 -0500

    Installs/updates tor_core for use by all bots

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,5 @@ default['tor'].tap do |attr|
   attr['tor_revision'] = 'CLOUD-DEPLOYMENT-TEST'
   attr['archivist_revision'] = 'CLOUD-DEPLOYMENT-TEST'
   attr['ocr_revision'] = 'CLOUD-DEPLOYMENT-TEST'
+  attr['core_revision'] = 'master'
 end
--- recipes/app.rb
+++ recipes/app.rb
@@ -55,6 +55,7 @@ template '/var/tor/praw.ini' do
   )
 end
 
+include_recipe 'tor::tor_core'
 include_recipe 'tor::tor_moderator'
 include_recipe 'tor::tor_archivist'
 include_recipe 'tor::tor_ocr'
