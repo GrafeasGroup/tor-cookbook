e6a46e313272b7319e380aa96900f312e26d2e56
David Alexander <opensource@thelonelyghost.com>
2018-05-10 16:47:35 -0400

    Extracts base OS config to separate cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,6 @@
 #
 # Copyright:: 2017, Grafeas Group, Ltd., All Rights Reserved.
 
-include_recipe "#{cookbook_name}::administration"
-include_recipe "#{cookbook_name}::redis"
-include_recipe "#{cookbook_name}::python"
-include_recipe "#{cookbook_name}::app"
+include_recipe '::redis'
+include_recipe '::python'
+include_recipe '::app'
